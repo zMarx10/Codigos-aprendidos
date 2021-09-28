e57887415f5f8b228d6d79c3aac01b19f6fbace9
zMarx10 <89613513+zMarx10@users.noreply.github.com>
2021-09-27 23:45:47 -0300

    Create Calculator.py

---
+++ Calculator/Calculator.py
@@ -0,0 +1,56 @@
+import time
+from colorama import init, Fore
+import os
+
+os.system('clear')
+
+print(Fore.GREEN+"""  __                              
+ /    _  |  _     |  _  |_  _   _ 
+ \__ (_| | (_ |_| | (_| |_ (_) |  
+                                  
+
+""")
+
+print("")
+print("             By Zmarx10         ")
+print("")
+print("")
+suma1 = int(input("Ingresa un numero: "))
+print("")
+suma2 = int(input("Ingresa un segundo numero: "))
+
+time.sleep(2)
+
+os.system('clear')
+
+
+
+print("=====================")
+print(f"(    sumando >         )")
+print("(                       )")
+print("(                       )")
+print("(     Cargando...       )")
+print("=====================")
+
+time.sleep(2)
+os.system('clear')
+print("=====================")
+print(f"(    sumando >  {suma1} + {suma2})")
+print("(                     )")
+print("(                     )")
+print("(     Cargando...     )")
+print("=====================")
+
+time.sleep(2)
+os.system('clear')
+print("=====================")
+print(f"(    sumando >  {suma1} + {suma2})")
+print("(                     )")
+print("(                     )")
+print("     resultado >      )")
+print(                 suma1+suma2)
+print("=====================")
+
+
+print("  Suma finalizada :3")
+time.sleep(1000)
