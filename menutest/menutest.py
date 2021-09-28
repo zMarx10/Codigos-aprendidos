fc0d3d81291c9d9b4267b1177e34fce1536d89a5
zMarx10 <89613513+zMarx10@users.noreply.github.com>
2021-09-28 15:35:12 -0300

    Create menutest.py

---
+++ menutest/menutest.py
@@ -0,0 +1,36 @@
+import os
+import time
+
+
+os.system("clear")
+print("==================")
+print("[Menu de opciones]")
+print("[ - - - - - - - -]")
+print("[                ]")
+print("[   opcion 1     ]")
+print("[   opcion 2     ]")
+print("[   opcion 3     ]")
+print("[   opcion 4     ]")
+print("==================")
+print("")
+
+op = int(input("Elige tu opcion: ") )
+
+
+
+if op == 1:
+    print("")
+    print("elegistes la opcion 1")
+    time.sleep(2)
+elif op == 2:
+    print("")
+    print("elegistes la opcion 2")
+    time.sleep(2)
+elif op == 3:
+    print("")
+    print("elegistes la opcion 3")
+    time.sleep(2)
+elif op == 4:
+    print("")
+    print("elegistes la opcion 4")
+    time.sleep(2)
